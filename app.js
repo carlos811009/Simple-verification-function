4412ccb95bb25f5a07fc339fe91efab8ceb319bf
carlos <carlos811009@gmail.com>
2021-06-13 14:15:28 +0800

    set cookie for keep the user login's status

--- app.js
+++ app.js
@@ -2,34 +2,41 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
+const cookieParser = require('cookie-parser')
 
 const users = [
   {
+    id: '001',
     firstName: 'Tony',
     email: 'tony@stark.com',
     password: 'iamironman'
   },
   {
+    id: '002',
     firstName: 'Steve',
     email: 'captain@hotmail.com',
     password: 'icandothisallday'
   },
   {
+    id: '003',
     firstName: 'Peter',
     email: 'peter@parker.com',
     password: 'enajyram'
   },
   {
+    id: '004',
     firstName: 'Natasha',
     email: 'natasha@gamil.com',
     password: '*parol#@$!'
   },
   {
+    id: '005',
     firstName: 'Nick',
     email: 'nick@shield.com',
     password: 'password'
   },
   {
+    id: '006',
     firstName: 'test',
     email: '123@123',
     password: '123'
@@ -41,6 +48,7 @@ const error_message = 'Username 或 Password 錯誤'
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(cookieParser())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
@@ -48,18 +56,34 @@ app.get('/', (req, res) => {
 })
 
 app.post('/user/login', (req, res) => {
+  // const cookieId = req.cookies('usercookie')
+  const userCookie = req.cookies.usercookie
   const { email, password } = req.body
   let i = 0
   users.forEach(user => {
     const authentication = user.email === email && user.password === password
+    if (user.id === userCookie) {
+      i++
+      console.log('login with cookie')
+      res.render('index', { user })
+      return
+    }
     if (authentication) {
       i++
-      return res.send(`${user.firstName} 歡迎回來`)
+      console.log('login with email')
+      res.cookie('usercookie', user.id)
+      res.render('index', { user })
+      return
     }
   })
   if (i === 0) {
     res.render('login', { error_message })
   }
+})
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('usercookie')
+  res.redirect('/')
 })
 
 app.listen(PORT, () => {
